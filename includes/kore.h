b6658ef11772cd0b41dd82ad6e7baafe0a6844a0
Joris Vink <joris@coders.se>
2014-12-12 19:36:56 +0100

    Bump to 1.2.1-develop

--- includes/kore.h
+++ includes/kore.h
@@ -51,7 +51,7 @@ extern int daemon(int, int);
 #define KORE_VERSION_MAJOR	1
 #define KORE_VERSION_MINOR	2
 #define KORE_VERSION_PATCH	1
-#define KORE_VERSION_STATE	"release"
+#define KORE_VERSION_STATE	"develop"
 
 #define errno_s			strerror(errno)
 #define ssl_errno_s		ERR_error_string(ERR_get_error(), NULL)
