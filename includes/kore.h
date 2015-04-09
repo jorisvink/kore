057924a5d576c9d327b435ed2d22a2a7ea3ae456
Joris Vink <joris@coders.se>
2015-04-09 11:45:17 +0200

    Move to 1.2.2-develop

--- includes/kore.h
+++ includes/kore.h
@@ -55,7 +55,7 @@ extern int daemon(int, int);
 #define KORE_VERSION_MAJOR	1
 #define KORE_VERSION_MINOR	2
 #define KORE_VERSION_PATCH	2
-#define KORE_VERSION_STATE	"release"
+#define KORE_VERSION_STATE	"develop"
 
 #define errno_s			strerror(errno)
 #define ssl_errno_s		ERR_error_string(ERR_get_error(), NULL)
