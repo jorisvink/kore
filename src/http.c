98148155dc0e4d3abd0387c359044eaea58605cd
Joris Vink <joris@coders.se>
2017-01-18 10:25:32 +0100

    bump copyright year

--- src/http.c
+++ src/http.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2016 Joris Vink <joris@coders.se>
+ * Copyright (c) 2013-2017 Joris Vink <joris@coders.se>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
