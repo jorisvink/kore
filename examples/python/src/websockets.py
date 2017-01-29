5485967547ddc8d6839512a5c3cceafc57d0cb76
Joris Vink <joris@coders.se>
2017-01-29 22:57:55 +0100

    add websocket example to python

---
+++ examples/python/src/websockets.py
@@ -0,0 +1,21 @@
+# using kore websockets via python.
+
+import kore
+
+def onconnect(c):
+	kore.log(kore.LOG_INFO, "%s: py connected" % c)
+
+def onmessage(c, op, data):
+	kore.log(kore.LOG_INFO, "%s: got message" % c)
+	kore.websocket_broadcast(c, op, data, kore.WEBSOCKET_BROADCAST_GLOBAL)
+
+def ondisconnect(c):
+	kore.log(kore.LOG_INFO, "%s: py disconnecting" % c)
+
+def ws_connect(req):
+	try:
+		req.websocket_handshake("onconnect", "onmessage", "ondisconnect")
+	except:
+		req.response(500, b'')
+
+	return kore.RESULT_OK
