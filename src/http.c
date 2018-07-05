47c1a1d195418047cb7ee35f60ae6c5b885e3c8f
Joris Vink <joris@coders.se>
2018-07-05 05:02:49 +0000

    set referer to NULL in http_request_new().

--- src/http.c
+++ src/http.c
@@ -1386,6 +1386,7 @@ http_request_new(struct connection *c, const char *host,
 	req->method = m;
 	req->hdlr = hdlr;
 	req->agent = NULL;
+	req->referer = NULL;
 	req->flags = flags;
 	req->fsm_state = 0;
 	req->http_body = NULL;
