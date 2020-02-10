6ba56bb8f6a9a3fec0815c1536100b22e7e6d3cd
Joris Vink <joris@coders.se>
2020-02-10 15:35:41 +0100

    adjust copyright years

--- src/runtime.c
+++ src/runtime.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Joris Vink <joris@coders.se>
+ * Copyright (c) 2017-2020 Joris Vink <joris@coders.se>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
